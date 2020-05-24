4a6995d4c9ab8f455c49f36440f2ab221a67cece
Douglas Schwe Pitangui <douglaspitangui@gmail.com>
2020-05-24 01:11:37 -0300

    Formularios - aulas 150,151,152 e 153

--- vue-components-avancado/src/App.vue
+++ vue-components-avancado/src/App.vue
@@ -1,7 +1,20 @@
 <template>
   <div id="app">
-    <h1>Vue Components</h1>
+    <h1>Vue JS</h1>
+    <Post>
+      <h2>componentes no vue</h2>
+      <P>compenes sao peças bla bla bla</P>
+    </Post>
   </div>
 </template>
 
+<script>
+import Post from './components/Post.vue'
 
+export default {
+  components:{
+    Post
+  }
+}
+
+</script>
